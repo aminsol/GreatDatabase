2907f7b603a1d3d143be491cccff7adf26ed95f1
molinab297 <molinab297@gmail.com>
2017-04-25 14:35:17 -0700

    Added Add Friend functionality
    changes to add friend functionality. Still need to do Remove Friend
    functionality

--- add-friend.php
+++ add-friend.php
@@ -1,15 +1,48 @@
-<html>
-<body
-  bgcolor="#E6E6FA"
-
 <?php
-    $db = new mysqli("localhost", "root", "", "project");
+
+  define("DB_HOST", "45.55.5.95");
+  define("DB_NAME", "thegreatdatabase");
+  define("DB_USER", "thegreatdatabase");
+  define("DB_PASS", "WeAreAwesome");
+  $db = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+    $sql = "select * from user";
+    $result = $db->query($sql);
+    $row = $result->fetch_assoc();
+    echo $row["email"]."<br/>";
+
+    // When user clicks submit button
+    if ( isset( $_POST['submit'] ) ) {
+
+      $sql = "select * from user where first like '{$_POST["first_name"]}' and last like '{$_POST["last_name"]}' ";
+      $result = $db->query($sql);
+      $row = $result->fetch_assoc();
+
+      // check if friend doesn't exist
+      if (!$row) {
+        // Add friend
+        $sql = "insert into user (first, last) values ('{$_POST["first_name"]}', '{$_POST["last_name"]}')";
+        if ($db->query($sql) === TRUE) {
+            echo "Added new friend successfully"."<br>";
+        } else {
+            echo "Error: " . $sql . "<br>" . $db->error;
+        }
+      }
+      else{
+        // Don't add friend
+        echo "Friend already exists"."<br>";
+      }
+  }
+
 ?>
 
+<html>
+<body
+  bgcolor="#FFC0CB"
   <h2>Add Friend</h2>
   <form action="" method="post">
-   <p>Your name: <input type="text" name="name" /></p>
-   <p>Your age: <input type="text" name="email" /></p>
+   <p>First name: <input type="text" name="first_name" /></p>
+   <p>Last name: <input type="text" name="last_name" /></p>
    <p><input type="submit" name="submit" /></p>
   </form>
 
