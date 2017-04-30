2c846192ab4ac3727f1c26b74d7cb67866c1d271
Amin Soltani <amin@aminsoltani.com>
2017-04-30 14:58:11 -0700

    wraping up stuff

--- login.php
+++ login.php
@@ -1,67 +1,61 @@
-<!--login.php-->
+<?php
+require_once 'config/config.php';
+$message = '';
+if(isset($_POST['submit'])){		//when user clicks submit
 
-	<?php 
-		//connect to database
-		define("DB_HOST", "45.55.5.95");
-		define("DB_NAME", "thegreatdatabase");
-		define("DB_USER", "thegreatdatabase");
-		define("DB_PASS", "WeAreAwesome");
-		$db = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+	//declare credentials
+	$email = $_POST["email"];
+	$password = $_POST["password"];
 
-		//check if successfully connected to database
-		if ($db->connect_error){
-			echo "ERROR"."<br/>";
-		}else{
-			echo "Welcome to the Great Database!"."<br/>";
-		}
+	//sql query string
+	$sql = "SELECT * FROM user WHERE email LIKE '$email' AND password LIKE '$password' ";
 
-		
+	//run query
+	$result = $db->query($sql);
+	$row = $result->fetch_assoc();
 
-		
-		if(isset($_POST['submit'])){		//when user clicks submit
+	//confirm query matches input
+	if ($row){
+		$message =  "Success"."<br/>";
+		include("home.php");
+	}else{
+		$message =   "Your email or password is incorrect, try again.";
+	}
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Welcome to the Great Database</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css" >
 
-		//declare credentials 
-		$email = $_POST["email"];
-		$password = $_POST["password"];
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="css/custom.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-		//sql query string
-		$sql = "SELECT * FROM user WHERE email LIKE '$email' AND password LIKE '$password' ";
 
-		//run query
-		$result = $db->query($sql);
-		$row = $result->fetch_assoc();
-
-			//confirm query matches input
-			if ($row){
-				echo "Success"."<br/>";
-				include("home.php");
-			}else{
-				echo "Your email or password is incorrect, try again.";
-			}
-		}
-	?>
-
-<html>
-	<head>
-		<title>LOGIN</title>
-		<link href="css/bootstrap.min.css" type="text/css" rel="stylesheet">
-		<style>
-			body {
-				text-align: center;
-				margin-top: 40%;
-			}
-		</style>
-	</head>
-	<body>
-		<div id="login">
-			<form action="" method="post" id="login">
-				<p>email: <input type="text" name="email"/></p>
-				<p>password: <input type="text" name="password"/><p>
-				<p><input type="submit" name="submit" value='login'/></p>
-			</form>
+</head>
+<body>
+<div class="container">
+<div class="login">
+	<h1 class="text-center">Welcome to the Great Databse</h1>
+	<form class="login" action="" method="post" id="login">
+		<div class="form-group">
+			<label for="usr">Email:</label>
+			<input type="email" name="email" class="form-control" id="usr">
 		</div>
-		<div id="register_link">
-			<p>Don't have an account? Register <a href="register.php">here.</a></p>
+		<div class="form-group">
+			<label for="pwd">Password:</label>
+			<input type="password" name="password" class="form-control" id="pwd">
 		</div>
-	</body>
-</html>
+		<p><input class="btn btn-success" type="submit" name="submit" value='Login'/></p>
+	</form>
+	<div class="text-danger"><?=$message ?></div>
+</div>
+</div>
+</body>
