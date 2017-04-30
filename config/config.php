8212c86f8f7e7bd4ff4bdd924d437f925a9a889c
Amin Soltani <amin@aminsoltani.com>
2017-04-30 11:16:48 -0700

    Adding conditiontion to make sure if $user is not defined we can redirect user to login page

--- config/config.php
+++ config/config.php
@@ -1,10 +1,14 @@
 <?php
-session_start();
-if(empty($_SESSION['login'])){
-    header('Location: login.php');
-}
 define("DB_HOST", "45.55.5.95");
 define("DB_NAME", "thegreatdatabase");
 define("DB_USER", "thegreatdatabase");
 define("DB_PASS", "WeAreAwesome");
 $db = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+}
+
+session_start();
+if(empty($_SESSION['login'])){
+    header('Location: login.php');
+}
